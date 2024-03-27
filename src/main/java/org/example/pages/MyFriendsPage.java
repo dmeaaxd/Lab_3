6884783681342cef1704756bcab841011a6d51c6
Daniil Kalinin <dan_kalinin_03@mail.ru>
2024-03-28 00:27:11 +0300

    resolve problems

--- src/main/java/org/example/pages/MyFriendsPage.java
+++ src/main/java/org/example/pages/MyFriendsPage.java
@@ -12,28 +12,27 @@ public class MyFriendsPage extends Page {
     }
 
     public void deleteFriend() {
-        WebElement buttonText = Utils.getElementBySelector(driver, By.xpath("//*[@id=\"content-column\"]/div/div[3]/div[1]/div/div/div/button/text()"));
-        if (!buttonText.getText().contains("Не дружить")) {
-            throw new IllegalStateException("Невозможно удалить друга, кнопка \"Не дружить\" отсутсвует");
-        }
         WebElement deleteFriendButton = Utils.getElementBySelector(driver, By.xpath("//*[@id=\"content-column\"]/div/div[3]/div[1]/div/div/div/button"));
-        deleteFriendButton.click();
-    }
-
-    public void addFriend() throws IllegalStateException{
-        WebElement buttonText = Utils.getElementBySelector(driver, By.xpath("//*[@id=\"content-column\"]/div/div[3]/div[1]/div/div/div/button/text()"));
-        if (!buttonText.getText().contains("Дружить")) {
-            throw new IllegalStateException("Невозможно добавить друга, кнопка \"Дружить\" отсутсвует");
+        String buttonText = deleteFriendButton.getText();
+        if (buttonText.contains("Не дружить") || buttonText.contains("Отменить")) {
+            deleteFriendButton.click();
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException ignored){}
+        } else {
+            throw new IllegalStateException("Невозможно удалить друга, кнопка \\\"Не дружить\\\" отсутсвует");
         }
-        WebElement addFriendButton = Utils.getElementBySelector(driver, By.xpath("//*[@id=\"content-column\"]/div/div[3]/div[1]/div/div/div/button"));
-        addFriendButton.click();
     }
 
-    public void confirmAddFriend() {
+    public void confirmAddFriend() throws IllegalStateException{
         WebElement incomingRequestsButton = Utils.getElementBySelector(driver, By.xpath("//*[@id=\"people-list-header-menu-item-incoming-requests\"]"));
         incomingRequestsButton.click();
         WebElement confirmRequestButton = Utils.getElementBySelector(driver, By.xpath("//*[@id=\"content-column\"]/div/div[2]/div[1]/div/div/div/button"));
-        confirmRequestButton.click();
+        if (confirmRequestButton != null)
+            confirmRequestButton.click();
+        else{
+            throw new IllegalStateException("Невозможно согласовать заявку, заявки отсутсвуют");
+        }
         try {
             Thread.sleep(3000);
         } catch (InterruptedException ignored){};
--- src/test/java/org/example/uc/MyFriendsPageTest.java
+++ src/test/java/org/example/uc/MyFriendsPageTest.java
@@ -2,6 +2,8 @@ package org.example.uc;
 
 import org.example.Utils;
 import org.example.pages.MainPage;
+import org.example.pages.MyFriendsPage;
+import org.example.pages.UserPage;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -11,6 +13,7 @@ import org.openqa.selenium.WebElement;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MyFriendsPageTest {
 
@@ -20,14 +23,95 @@ public class MyFriendsPageTest {
     }
 
     @Test
+    void confirmFriendRequestTest() {
+        sendFriendRequest();
+        List<WebDriver> drivers = Utils.getDrivers();
+        drivers.parallelStream().forEach(webDriver -> {
+            MainPage mainPage = new MainPage(webDriver);
+            webDriver.get(Utils.BASE_URL);
+            mainPage.doSecondUserLogin();
+            WebElement burgerMenuButton = Utils.getElementBySelector(webDriver, By.xpath("//*[@id=\"header-profile-tooltip\"]/button"));
+            burgerMenuButton.click();
+            WebElement myFriendsButton = Utils.getElementBySelector(webDriver, By.xpath("/html/body/div[3]/div/div/div/a[7]"));
+            myFriendsButton.click();
+            MyFriendsPage myFriendsPage = new MyFriendsPage(webDriver);
+            myFriendsPage.confirmAddFriend();
+            WebElement addFriendButton = Utils.getElementBySelector(webDriver, By.xpath("//*[@id=\"content-column\"]/div/div[2]/div[1]/div/div/div/button"));
+            String buttonText = addFriendButton.getText();
+            assertTrue(buttonText.contains("Не дружить"));
+        });
+        drivers.forEach(WebDriver::quit);
+    }
+
+    @Test
     public void deleteFriendTest() {
+        sendFriendRequest();
+        confirmFriendRequest();
         List<WebDriver> drivers = Utils.getDrivers();
         drivers.parallelStream().forEach(webDriver -> {
             MainPage mainPage = new MainPage(webDriver);
             webDriver.get(Utils.BASE_URL);
             mainPage.doFirstUserLogin();
-            WebElement burgerMenu = Utils.getElementBySelector(webDriver, By.xpath("//*[@id=\"header-profile-tooltip\"]/button"));
-            assertNotNull(burgerMenu);
+            WebElement burgerMenuButton = Utils.getElementBySelector(webDriver, By.xpath("//*[@id=\"header-profile-tooltip\"]/button"));
+            burgerMenuButton.click();
+            WebElement myFriendsButton = Utils.getElementBySelector(webDriver, By.xpath("/html/body/div[3]/div/div/div/a[7]"));
+            myFriendsButton.click();
+            MyFriendsPage myFriendsPage = new MyFriendsPage(webDriver);
+            myFriendsPage.deleteFriend();
+            WebElement deleteFriendButton = Utils.getElementBySelector(webDriver, By.xpath("//*[@id=\"content-column\"]/div/div[3]/div[1]/div/div/div/button"));
+            String buttonText = deleteFriendButton.getText();
+            assertTrue(buttonText.contains("Дружить"));
+        });
+        drivers.forEach(WebDriver::quit);
+    }
+
+    void sendFriendRequest() {
+        List<WebDriver> drivers = Utils.getDrivers();
+        drivers.parallelStream().forEach(webDriver -> {
+            MainPage mainPage = new MainPage(webDriver);
+            webDriver.get(Utils.BASE_URL);
+            mainPage.doFirstUserLogin();
+            Utils.waitUntilPageLoads(webDriver,10);
+            UserPage userPage = new UserPage(webDriver);
+            webDriver.get(Utils.SECOND_USER_URL);
+            Utils.waitUntilPageLoads(webDriver,10);
+
+            WebElement button = Utils.getElementBySelector(webDriver, By.xpath("//*[@id=\"secondary-header\"]/div/div[3]/button"));
+            String buttonText = button.getText();
+            if (buttonText.contains("Не дружить")){
+                button.click();
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException ignored){}
+            }
+            if (!buttonText.contains("Отменить")){
+                userPage.addFriend();
+                WebElement addFriendButton = Utils.getElementBySelector(webDriver, By.xpath("//*[@id=\"secondary-header\"]/div/div[3]/button"));
+                buttonText = addFriendButton.getText();
+            }
+            assertTrue(buttonText.contains("Отменить"));
+        });
+        drivers.forEach(WebDriver::quit);
+    }
+
+    void confirmFriendRequest() {
+        List<WebDriver> drivers = Utils.getDrivers();
+        drivers.parallelStream().forEach(webDriver -> {
+            MainPage mainPage = new MainPage(webDriver);
+            webDriver.get(Utils.BASE_URL);
+            mainPage.doSecondUserLogin();
+            WebElement burgerMenuButton = Utils.getElementBySelector(webDriver, By.xpath("//*[@id=\"header-profile-tooltip\"]/button"));
+            burgerMenuButton.click();
+            WebElement myFriendsButton = Utils.getElementBySelector(webDriver, By.xpath("/html/body/div[3]/div/div/div/a[7]"));
+            myFriendsButton.click();
+            MyFriendsPage myFriendsPage = new MyFriendsPage(webDriver);
+            try {
+                myFriendsPage.confirmAddFriend();
+                WebElement addFriendButton = Utils.getElementBySelector(webDriver, By.xpath("//*[@id=\"content-column\"]/div/div[2]/div[1]/div/div/div/button"));
+                String buttonText = addFriendButton.getText();
+                assertTrue(buttonText.contains("Не дружить"));
+            } catch (IllegalStateException ignored){}
+
         });
         drivers.forEach(WebDriver::quit);
     }
