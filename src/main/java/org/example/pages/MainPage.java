fbc589b7c840170434b1e4a56912277cbe6a7645
Daniil Kalinin <dan_kalinin_03@mail.ru>
2024-03-27 20:30:14 +0300

    MainPage update paths

--- src/main/java/org/example/pages/MainPage.java
+++ src/main/java/org/example/pages/MainPage.java
@@ -20,22 +20,22 @@ public class MainPage extends Page {
     }
 
     public void doLogout() {
-        WebElement burgerMenuButton = Utils.getElementBySelector(driver, By.xpath(""));
+        WebElement burgerMenuButton = Utils.getElementBySelector(driver, By.xpath("//*[@id=\"header-profile-tooltip\"]/button"));
         burgerMenuButton.click();
-        WebElement logoutButton = Utils.getElementBySelector(driver, By.xpath(""));
+        WebElement logoutButton = Utils.getElementBySelector(driver, By.xpath("/html/body/div[3]/div/div/div/a[13]"));
         logoutButton.click();
-        WebElement confirmationLogoutButton = Utils.getElementBySelector(driver, By.xpath(""));
-        confirmationLogoutButton.click();
+        WebElement confirmLogoutButton = Utils.getElementBySelector(driver, By.xpath("/html/body/div[3]/div/div/div/div/button"));
+        confirmLogoutButton.click();
     }
 
     private void tryLogin(CharSequence login, CharSequence password) {
-        WebElement loginButton = Utils.getElementBySelector(driver, By.xpath(""));
+        WebElement loginButton = Utils.getElementBySelector(driver, By.xpath("//*[@id=\"header\"]/div/div/div/div/div[4]/ul/li[2]/button"));
         loginButton.click();
-        WebElement loginByEmailButton = Utils.getElementBySelector(driver, By.xpath(""));
+        WebElement loginByEmailButton = Utils.getElementBySelector(driver, By.xpath("/html/body/div[3]/div/div/div/div/div/form/div[6]/button"));
         loginByEmailButton.click();
-        WebElement loginInput = Utils.getElementBySelector(driver, By.xpath(""));
-        WebElement loginPassword = Utils.getElementBySelector(driver, By.xpath(""));
-        WebElement authButton = Utils.getElementBySelector(driver, By.xpath(""));
+        WebElement loginInput = Utils.getElementBySelector(driver, By.xpath("//*[@id=\"authFormLoginByEmailEmail\"]"));
+        WebElement loginPassword = Utils.getElementBySelector(driver, By.xpath("//*[@id=\"authFormLoginByEmailPassword\"]"));
+        WebElement authButton = Utils.getElementBySelector(driver, By.xpath("/html/body/div[3]/div/div/div/div/div/form/div[3]/button"));
         loginInput.clear();
         loginPassword.clear();
         loginInput.sendKeys(login);
