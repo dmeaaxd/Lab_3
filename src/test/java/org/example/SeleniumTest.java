dd90f7edb5a4d6f2ea310fe0cae561190ee1258e
Daniil Kalinin <dan_kalinin_03@mail.ru>
2024-03-27 20:35:44 +0300

    add SeleniumTest

---
+++ src/test/java/org/example/SeleniumTest.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SeleniumTest {
+
+    @BeforeAll
+    static void prepareDrivers() {
+        Utils.prepareDrivers();
+    }
+
+    @Test
+    void testDriver() {
+        Utils.getDrivers().forEach(this::executeWithCapabilities);
+    }
+
+    private void executeWithCapabilities(WebDriver driver) {
+        driver.get(Utils.BASE_URL);
+        String title = driver.getTitle();
+        assertEquals("Лента - Новости - Медиаплатформа МирТесен", title);
+        driver.quit();
+    }
+}
